f48a8ec6551ec0496756e6c0fbf23281d7c9a733
Abror Abdan Syakuron <abror.abdan.dev@gmail.com>
2025-01-02 23:58:23 +0800

    feat: add task management with completed and undo functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener("dblclick", (e)=> {
     document.documentElement.requestFullscreen(); 
 })
 
+
+//Untuk memuncukan container add task
 const addShow = document.getElementById("add-show");
 const addContainer = document.getElementById("add-container");
 const dark = document.getElementById("dark");
@@ -12,6 +14,70 @@ addShow.addEventListener("click", function(){
 })
 
 
+//Menambah task
+let taskList = [];
+const inputTask = document.getElementById("input-task");
+const addTask = document.getElementById("add-task")
+
+addTask.addEventListener("click", function(e){
+    const divTask = document.createElement("div");
+    divTask.setAttribute("class", "task");
+    divTask.setAttribute("data-tindex", `${taskList.length}`)
+
+    divTask.innerHTML =`
+                        <input type="checkbox">
+                        <p>${inputTask.value}</p>`
+
+    taskList.push(divTask);
+
+    inputTask.value = "";
+    addContainer.style.display = "none";
+    dark.style.display = "none";
+
+    
+    displayTask();
+
+    //Completed Task
+    const taskCheck = divTask.querySelector(`input[type="checkbox"]`);
+    taskCheck.addEventListener("click", function(){
+        if(taskCheck.checked == true){
+            completedList.push(divTask.dataset.tindex);
+            // delete taskList[divTask.dataset.tindex];
+            console.log(divTask.dataset.tindex)
+            
+        }else{
+            console.log(divTask.dataset.tindex)
+            delete completedList[completedList.indexOf(divTask.dataset.tindex)];
+            completedList = completedList.filter(function(item){
+                return item != undefined;
+            })
+        }
+        displayTask();
+    })
+})
+
+//Completed Task
+let completedList = [];
+const todayTask = document.getElementById("today-task");
+const completedTask = document.getElementById("completed-task");
+
+
+//Display task
+function displayTask(){
+    completedTask.innerHTML = "";
+    todayTask.innerHTML = "";
+
+    for(let i = 0; i < taskList.length; i++){
+        if(completedList.includes(taskList[i].dataset.tindex)){
+            completedTask.appendChild(taskList[i]);
+        }else{
+            todayTask.appendChild(taskList[i]);
+        }
+    }
+}
+
+
+//change or add category
 const changeCategory = document.getElementById("change-container");
 
 categoryContainer = document.querySelector("#change-container div");
@@ -47,6 +113,8 @@ addCategory.addEventListener("click", function(){
     createCategoryContainer.style.display = "block";
 })
 
+
+//Untuk display none
 document.addEventListener("click", function(e){
     if(createCategoryContainer.style.display == "none"){
         if(!addContainer.contains(e.target) && e.target != addShow){
