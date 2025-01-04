d076b6b3df36a2106f29ac88a075cd83d5d8da60
Abror Abdan Syakuron <abror.abdan.dev@gmail.com>
2025-01-05 01:53:40 +0800

    save task list to localStorage

--- index.js
+++ index.js
@@ -14,37 +14,78 @@ addShow.addEventListener("click", function(){
 })
 
 
+let taskList = getLocalStorage("localTask") || [];
+let completedList = getLocalStorage("localCompleted") || [];
+
+
+
+
 //Menambah task
-let taskList = [];
 const inputTask = document.getElementById("input-task");
 const addTask = document.getElementById("add-task")
 
 addTask.addEventListener("click", function(e){
-    const divTask = document.createElement("div");
-    divTask.setAttribute("class", "task");
-    divTask.setAttribute("data-tindex", `${taskList.length}`)
+    // const divTask = document.createElement("div");
+    // divTask.setAttribute("class", "task");
+    // divTask.setAttribute("data-tindex", `${taskList.length}`)
 
-    divTask.innerHTML =`
-                        <input type="checkbox">
-                        <p>${inputTask.value}</p>`
+    // divTask.innerHTML =`
+    //                     <input type="checkbox">
+    //                     <p>${inputTask.value}</p>`
 
-    taskList.push(divTask);
+    // taskList.push(divTask);
+
+    createTaskDiv(inputTask.value, taskList.length);
+    taskList.push(inputTask.value);
 
     inputTask.value = "";
     addContainer.style.display = "none";
     dark.style.display = "none";
 
     
-    displayTask();
+    // displayTask();
+})
+//Container task
+const todayTask = document.getElementById("today-task");
+const completedTask = document.getElementById("completed-task");
 
-    //Completed Task
+//Display task
+function displayTask(){
+    completedTask.innerHTML = "";
+    todayTask.innerHTML = "";
+    
+    for(let i = 0; i < taskDivList.length; i++){
+        if(completedList.includes(taskDivList[i].dataset.tindex)){
+            const taskCheck = taskDivList[i].querySelector(`input[type="checkbox"]`);
+            taskCheck.checked = true;
+            completedTask.appendChild(taskDivList[i]);
+        }else{
+            todayTask.appendChild(taskDivList[i]);
+        }
+    }
+}
+
+
+//Membuat taskDiv
+let taskDivList = [];
+function createTaskDiv(value, index){
+    const divTask = document.createElement("div");
+    divTask.setAttribute("class", "task");
+    divTask.setAttribute("data-tindex", `${index}`);
+    
+    divTask.innerHTML =`
+    <input type="checkbox">
+    <p>${value}</p>`
+    
+    taskDivList.push(divTask);
+
+    // Completed Task
     const taskCheck = divTask.querySelector(`input[type="checkbox"]`);
     taskCheck.addEventListener("click", function(){
+        console.log("clicked")
         if(taskCheck.checked == true){
             completedList.push(divTask.dataset.tindex);
-            // delete taskList[divTask.dataset.tindex];
             console.log(divTask.dataset.tindex)
-            
         }else{
             console.log(divTask.dataset.tindex)
             delete completedList[completedList.indexOf(divTask.dataset.tindex)];
@@ -54,28 +95,22 @@ addTask.addEventListener("click", function(e){
         }
         displayTask();
     })
-})
 
-//Completed Task
-let completedList = [];
-const todayTask = document.getElementById("today-task");
-const completedTask = document.getElementById("completed-task");
-
-
-//Display task
-function displayTask(){
-    completedTask.innerHTML = "";
-    todayTask.innerHTML = "";
-
-    for(let i = 0; i < taskList.length; i++){
-        if(completedList.includes(taskList[i].dataset.tindex)){
-            completedTask.appendChild(taskList[i]);
-        }else{
-            todayTask.appendChild(taskList[i]);
-        }
-    }
+    displayTask();
 }
 
+//Menampilkan task awal dari local storage
+// let indexTemp = 0;
+// taskList.forEach(element => {
+//     console.log(taskList)
+//     console.log(element)
+//     createTaskDiv(element, indexTemp);
+//     indexTemp++;
+// });
+
+for(let i = 0; i < taskList.length; i++){
+    createTaskDiv(taskList[i], i);
+}
 
 //change or add category
 const changeCategory = document.getElementById("change-container");
@@ -131,4 +166,25 @@ document.addEventListener("click", function(e){
             createCategoryContainer.style.display = "none";
         }
     }
+})
+
+
+function getLocalStorage(key){
+    if(localStorage.getItem(key) != null && localStorage.getItem(key) != undefined){
+        
+        return JSON.parse(localStorage.getItem(key));
+    }
+
+    return null;
+}
+//Untuk menyimpan data
+addEventListener("beforeunload", function(e){
+    window.localStorage.removeItem("localTask");
+    window.localStorage.removeItem("localCompleted");
+    if(taskList.length != 0){
+        console.log(taskList)
+        localStorage.setItem("localTask", JSON.stringify(taskList));
+        localStorage.setItem("localCompleted", JSON.stringify(completedList));
+    }
+    
 })
